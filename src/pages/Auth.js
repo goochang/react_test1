fa079986726029c9f7785e724af75889e1afb68d
goochang <cck742@naver.com>
2021-06-24 20:49:23 +0900

    visible header

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as baseActions from 'redux/modules/base';
 
 class Auth extends Component {
+
+    componentWillMount() {
+        this.props.BaseActions.setHeaderVisibility(false);
+    }
+
+    componentWillUnmount() {
+        this.props.BaseActions.setHeaderVisibility(true);
+    }
+
     render() {
         return (
             <div>
@@ -10,4 +22,11 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+export default connect(
+    (state) => ({
+
+    }),
+    (dispatch) => ({
+        BaseActions: bindActionCreators(baseActions, dispatch)
+    })
+)(Auth);

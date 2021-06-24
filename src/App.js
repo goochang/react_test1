4b49e590e94d2fe88790c73aa88e978c138348ce
goochang <cck742@naver.com>
2021-06-24 20:18:27 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { Home, Auth } from './pages';
+import { Home, Auth } from 'pages';
+import { hot } from 'react-hot-loader/root'
+import HeaderContainer from 'containers/Base/HeaderContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div>
+        <HeaderContainer />
         <Route exact path="/" component={Home} />
         <Route path="/auth" component={Auth} />
       </div>
@@ -13,4 +17,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default process.env.NODE_ENV === "development" ? hot(App) : App
